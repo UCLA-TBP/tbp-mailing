21c8191250968003f50a06d6586937d97a192081
Vincent <vircheng@gmail.com>
2018-02-01 15:15:43 -0800

    Cleaned up configs and added database init on startup.

--- tbp-postfix/init.sh
+++ tbp-postfix/init.sh
@@ -7,6 +7,12 @@ cp -r /etc/hosts /etc/localtime /etc/resolv.conf /etc/services /etc/ssl $PFROOT/
 chown root:root $PFROOT
 chown root:root $PFROOT/pid
 
+postmap /etc/postfix/virtual
+postmap /etc/postfix/restrict_classes/tbponly
+postmap /etc/postfix/restrict_classes/houseleaders
+postmap /etc/postfix/access_restrictions
+newaliases
+
 postfix start
 
 /bin/sh
