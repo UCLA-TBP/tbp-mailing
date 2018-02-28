48929b0cd74bcfd3d4f8355199cf429663367847
Vincent <vircheng@gmail.com>
2018-02-28 01:19:50 -0800

    Add virtual mailbox support and fix various bugs
    - Restarting a container no longer duplicates logging output
    - Dovecot no longer attempts to associate users with UNIX accounts

--- tbp-postfix/init.sh
+++ tbp-postfix/init.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
 
-#set up logging support
-touch /etc/syslog.conf
+#set up logging support 
 touch /var/log/postfix.info /var/log/postfix.warning /var/log/postfix.err /var/log/postfix.crit
-echo -e "mail.crit\t/var/log/postfix.crit" >> /etc/syslog.conf
-echo -e "mail.err\t/var/log/postfix.err" >> /etc/syslog.conf
-echo -e "mail.warning\t/var/log/postfix.warning" >> /etc/syslog.conf
-echo -e "mail.info\t/var/log/postfix.info" >> /etc/syslog.conf
-echo -e "mail.debug\t/var/log/postfix.debug" >> /etc/syslog.conf
 syslogd
 
 #set up chroot environment
@@ -19,8 +13,14 @@ cp -r /etc/hosts /etc/localtime /etc/resolv.conf /etc/services /etc/ssl /etc/sys
 chown root:root $PFROOT
 chown root:root $PFROOT/pid
 
+#set up virtual mailboxes
+VMAIL=/var/mail/vhosts
+mkdir -p $VMAIL
+chown -R postfix:postfix $VMAIL
+
 #create postfix databases
 postmap /etc/postfix/virtual
+postmap /etc/postfix/vmailbox
 postmap /etc/postfix/restrict_classes/tbponly
 postmap /etc/postfix/restrict_classes/houseleaders
 postmap /etc/postfix/access_restrictions
