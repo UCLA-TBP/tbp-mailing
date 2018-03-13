2e3cec587e2f3719d56a594022561b8ada501d0a
vrcheng <vircheng@gmail.com>
2018-03-13 20:23:25 +0000

    Minor fix

--- migrate_confs.sh
+++ migrate_confs.sh
@@ -7,3 +7,4 @@ cp $CONF/virtual $DEST/virtual
 cp $CONF/vmailbox $DEST/vmailbox
 cp $CONF/restrict_classes/houseleaders $DEST/restrict_classes/houseleaders
 cp $CONF/restrict_classes/tbponly $DEST/restrict_classes/tbponly
+
